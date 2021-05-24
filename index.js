aa31ad710f81e5eb4d58303e6ca2ebe7996356e1
WCL <WCL>
2021-05-24 14:50:46 +0800

    update

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
+/*
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
+const SMSServiceURL = 'https://sms.emma.hk/sms/APIServiceMulti'
 
-app.get('/', (req, res) => {
+app.get('/SendMessage', (req, res) => {
+  const request = require('request');
+  var correlationId = req.query.correlationId;
+request('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  console.log(body.url);
+  console.log(body.explanation);
+  console.log(correlationId);
+});
   res.send('Hello World!')
 })
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`)
 })
+*/
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
