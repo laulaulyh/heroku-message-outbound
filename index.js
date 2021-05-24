b1d0510c5522974770447fbe70b6108aebfa12e1
WCL <WCL>
2021-05-24 11:06:57 +0800

    update

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
-const path = require('path')
+const app = express()
 const PORT = process.env.PORT || 5000
 
-express()
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
